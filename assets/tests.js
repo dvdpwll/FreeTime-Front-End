b8a3570652bcd574d5e1aef1243e1abeb76a3301
dvdpwll <dvdpwll456@gmail.com>
2016-09-28 10:14:50 -0400

    add dropdowns
    changed day, start, and end for both new schedule and edit schedule
    to dropdowns

--- assets/tests.js
+++ assets/tests.js
@@ -221,6 +221,316 @@ define('dvdpwll.FreeTime-Front-End/tests/helpers/destroy-app.jshint', ['exports'
     assert.ok(true, 'helpers/destroy-app.js should pass jshint.');
   });
 });
+define('dvdpwll.FreeTime-Front-End/tests/helpers/ember-basic-dropdown', ['exports', 'ember', 'ember-runloop'], function (exports, _ember, _emberRunloop) {
+  exports.nativeClick = nativeClick;
+  exports.clickTrigger = clickTrigger;
+  exports.tapTrigger = tapTrigger;
+  exports.fireKeydown = fireKeydown;
+
+  // integration helpers
+  function focus(el) {
+    if (!el) {
+      return;
+    }
+    var $el = jQuery(el);
+    if ($el.is(':input, [contenteditable=true]')) {
+      var type = $el.prop('type');
+      if (type !== 'checkbox' && type !== 'radio' && type !== 'hidden') {
+        (0, _emberRunloop['default'])(null, function () {
+          // Firefox does not trigger the `focusin` event if the window
+          // does not have focus. If the document doesn't have focus just
+          // use trigger('focusin') instead.
+
+          if (!document.hasFocus || document.hasFocus()) {
+            el.focus();
+          } else {
+            $el.trigger('focusin');
+          }
+        });
+      }
+    }
+  }
+
+  function nativeClick(selector) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    var mousedown = new window.Event('mousedown', { bubbles: true, cancelable: true, view: window });
+    var mouseup = new window.Event('mouseup', { bubbles: true, cancelable: true, view: window });
+    var click = new window.Event('click', { bubbles: true, cancelable: true, view: window });
+    Object.keys(options).forEach(function (key) {
+      mousedown[key] = options[key];
+      mouseup[key] = options[key];
+      click[key] = options[key];
+    });
+    var element = document.querySelector(selector);
+    (0, _emberRunloop['default'])(function () {
+      return element.dispatchEvent(mousedown);
+    });
+    focus(element);
+    (0, _emberRunloop['default'])(function () {
+      return element.dispatchEvent(mouseup);
+    });
+    (0, _emberRunloop['default'])(function () {
+      return element.dispatchEvent(click);
+    });
+  }
+
+  function clickTrigger(scope) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    var selector = '.ember-basic-dropdown-trigger';
+    if (scope) {
+      selector = scope + ' ' + selector;
+    }
+    nativeClick(selector, options);
+  }
+
+  function tapTrigger(scope) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    var selector = '.ember-basic-dropdown-trigger';
+    if (scope) {
+      selector = scope + ' ' + selector;
+    }
+    var touchStartEvent = new window.Event('touchstart', { bubbles: true, cancelable: true, view: window });
+    Object.keys(options).forEach(function (key) {
+      return touchStartEvent[key] = options[key];
+    });
+    (0, _emberRunloop['default'])(function () {
+      return document.querySelector(selector).dispatchEvent(touchStartEvent);
+    });
+    var touchEndEvent = new window.Event('touchend', { bubbles: true, cancelable: true, view: window });
+    Object.keys(options).forEach(function (key) {
+      return touchEndEvent[key] = options[key];
+    });
+    (0, _emberRunloop['default'])(function () {
+      return document.querySelector(selector).dispatchEvent(touchEndEvent);
+    });
+  }
+
+  function fireKeydown(selector, k) {
+    var oEvent = document.createEvent('Events');
+    oEvent.initEvent('keydown', true, true);
+    $.extend(oEvent, {
+      view: window,
+      ctrlKey: false,
+      altKey: false,
+      shiftKey: false,
+      metaKey: false,
+      keyCode: k,
+      charCode: k
+    });
+    (0, _emberRunloop['default'])(function () {
+      return document.querySelector(selector).dispatchEvent(oEvent);
+    });
+  }
+
+  // acceptance helpers
+
+  exports['default'] = function () {
+    _ember['default'].Test.registerAsyncHelper('clickDropdown', function (app, cssPath) {
+      var options = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+
+      clickTrigger(cssPath, options);
+    });
+
+    _ember['default'].Test.registerAsyncHelper('tapDropdown', function (app, cssPath) {
+      var options = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+
+      tapTrigger(cssPath, options);
+    });
+  };
+});
+define('dvdpwll.FreeTime-Front-End/tests/helpers/ember-power-select', ['exports', 'jquery', 'ember-runloop', 'ember-test'], function (exports, _jquery, _emberRunloop, _emberTest) {
+  exports.nativeMouseDown = nativeMouseDown;
+  exports.nativeMouseUp = nativeMouseUp;
+  exports.triggerKeydown = triggerKeydown;
+  exports.typeInSearch = typeInSearch;
+  exports.clickTrigger = clickTrigger;
+  exports.nativeTouch = nativeTouch;
+  exports.touchTrigger = touchTrigger;
+
+  // Helpers for integration tests
+
+  function typeText(selector, text) {
+    var $selector = (0, _jquery['default'])((0, _jquery['default'])(selector).get(0)); // Only interact with the first result
+    $selector.val(text);
+    var event = document.createEvent('Events');
+    event.initEvent('input', true, true);
+    $selector[0].dispatchEvent(event);
+  }
+
+  function fireNativeMouseEvent(eventType, selectorOrDomElement) {
+    var options = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+
+    var event = new window.Event(eventType, { bubbles: true, cancelable: true, view: window });
+    Object.keys(options).forEach(function (key) {
+      return event[key] = options[key];
+    });
+    var target = undefined;
+    if (typeof selectorOrDomElement === 'string') {
+      target = (0, _jquery['default'])(selectorOrDomElement)[0];
+    } else {
+      target = selectorOrDomElement;
+    }
+    (0, _emberRunloop['default'])(function () {
+      return target.dispatchEvent(event);
+    });
+  }
+
+  function nativeMouseDown(selectorOrDomElement, options) {
+    fireNativeMouseEvent('mousedown', selectorOrDomElement, options);
+  }
+
+  function nativeMouseUp(selectorOrDomElement, options) {
+    fireNativeMouseEvent('mouseup', selectorOrDomElement, options);
+  }
+
+  function triggerKeydown(domElement, k) {
+    var oEvent = document.createEvent('Events');
+    oEvent.initEvent('keydown', true, true);
+    _jquery['default'].extend(oEvent, {
+      view: window,
+      ctrlKey: false,
+      altKey: false,
+      shiftKey: false,
+      metaKey: false,
+      keyCode: k,
+      charCode: k
+    });
+    (0, _emberRunloop['default'])(function () {
+      domElement.dispatchEvent(oEvent);
+    });
+  }
+
+  function typeInSearch(text) {
+    (0, _emberRunloop['default'])(function () {
+      typeText('.ember-power-select-search-input, .ember-power-select-search input, .ember-power-select-trigger-multiple-input, input[type="search"]', text);
+    });
+  }
+
+  function clickTrigger(scope) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    var selector = '.ember-power-select-trigger';
+    if (scope) {
+      selector = scope + ' ' + selector;
+    }
+    nativeMouseDown(selector, options);
+  }
+
+  function nativeTouch(selectorOrDomElement) {
+    var event = new window.Event('touchstart', { bubbles: true, cancelable: true, view: window });
+    var target = undefined;
+
+    if (typeof selectorOrDomElement === 'string') {
+      target = (0, _jquery['default'])(selectorOrDomElement)[0];
+    } else {
+      target = selectorOrDomElement;
+    }
+    (0, _emberRunloop['default'])(function () {
+      return target.dispatchEvent(event);
+    });
+    (0, _emberRunloop['default'])(function () {
+      event = new window.Event('touchend', { bubbles: true, cancelable: true, view: window });
+      target.dispatchEvent(event);
+    });
+  }
+
+  function touchTrigger() {
+    var selector = '.ember-power-select-trigger';
+    nativeTouch(selector);
+  }
+
+  // Helpers for acceptance tests
+
+  exports['default'] = function () {
+    _emberTest['default'].registerAsyncHelper('selectChoose', function (app, cssPath, value) {
+      var $trigger = find(cssPath + ' .ember-power-select-trigger');
+
+      if ($trigger === undefined || $trigger.length === 0) {
+        $trigger = find(cssPath);
+      }
+
+      var contentId = '' + $trigger.attr('aria-controls');
+      var $content = find('#' + contentId);
+      // If the dropdown is closed, open it
+      if ($content.length === 0) {
+        nativeMouseDown($trigger.get(0));
+        wait();
+      }
+
+      // Select the option with the given text
+      andThen(function () {
+        var potentialTargets = (0, _jquery['default'])('#' + contentId + ' .ember-power-select-option:contains("' + value + '")').toArray();
+        var target = undefined;
+        if (potentialTargets.length > 1) {
+          target = potentialTargets.filter(function (t) {
+            return t.textContent.trim() === value;
+          })[0] || potentialTargets[0];
+        } else {
+          target = potentialTargets[0];
+        }
+        nativeMouseUp(target);
+      });
+    });
+
+    _emberTest['default'].registerAsyncHelper('selectSearch', function (app, cssPath, value) {
+      var triggerPath = cssPath + ' .ember-power-select-trigger';
+      var $trigger = find(triggerPath);
+      if ($trigger === undefined || $trigger.length === 0) {
+        triggerPath = cssPath;
+        $trigger = find(triggerPath);
+      }
+
+      var contentId = '' + $trigger.attr('aria-controls');
+      var isMultipleSelect = (0, _jquery['default'])(cssPath + ' .ember-power-select-trigger-multiple-input').length > 0;
+
+      var dropdownIsClosed = (0, _jquery['default'])('#' + contentId).length === 0;
+      if (dropdownIsClosed) {
+        nativeMouseDown(triggerPath);
+        wait();
+      }
+      var isDefaultSingleSelect = (0, _jquery['default'])('.ember-power-select-search-input').length > 0;
+
+      if (isMultipleSelect) {
+        fillIn(triggerPath + ' .ember-power-select-trigger-multiple-input', value);
+      } else if (isDefaultSingleSelect) {
+        fillIn('.ember-power-select-search-input', value);
+      } else {
+        // It's probably a customized version
+        var inputIsInTrigger = !!find(cssPath + ' .ember-power-select-trigger input[type=search]')[0];
+        if (inputIsInTrigger) {
+          fillIn(triggerPath + ' input[type=search]', value);
+        } else {
+          fillIn('#' + contentId + ' .ember-power-select-search-input[type=search]', 'input');
+        }
+      }
+    });
+
+    _emberTest['default'].registerAsyncHelper('removeMultipleOption', function (app, cssPath, value) {
+      var elem = find(cssPath + ' .ember-power-select-multiple-options > li:contains(' + value + ') > .ember-power-select-multiple-remove-btn').get(0);
+      try {
+        nativeMouseDown(elem);
+        wait();
+      } catch (e) {
+        console.warn('css path to remove btn not found');
+        throw e;
+      }
+    });
+
+    _emberTest['default'].registerAsyncHelper('clearSelected', function (app, cssPath) {
+      var elem = find(cssPath + ' .ember-power-select-clear-btn').get(0);
+      try {
+        nativeMouseDown(elem);
+        wait();
+      } catch (e) {
+        console.warn('css path to clear btn not found');
+        throw e;
+      }
+    });
+  };
+});
 define('dvdpwll.FreeTime-Front-End/tests/helpers/flash-message', ['exports', 'ember', 'ember-cli-flash/flash/object'], function (exports, _ember, _emberCliFlashFlashObject) {
   var K = _ember['default'].K;
 
